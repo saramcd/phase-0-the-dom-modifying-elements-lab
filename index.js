923cc9a9a5d7207ad7078a8f95883ffa6e3a64ec
Sara McDonnell <saramcdonnell32@gmail.com>
2023-01-30 20:18:30 -0500

    lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const mainToRemove = document.getElementById("main");
+mainToRemove.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+
+newHeader.textContent = "SARA is the champion";
